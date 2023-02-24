1f14f0c6714209733d10f24b62378f20b77fa7f9
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-02-24 09:23:45 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Chris Markiewicz on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
